0e850255dab057bb57d4334cb09a2af57d021ff0
jeffrey <jeffrey.vt@hotmail.com>
2017-04-28 22:58:24 +1000

    fixed the sendrecv.c

--- week8/sendrecv/sendrecv.c
+++ week8/sendrecv/sendrecv.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[]){
 	int rc,rank, size, dest, source, input, tag=1;
-	int a=0,b=0;
+	int value, rec_val;
 	
 	MPI_Status Stat;
 	MPI_Init(&argc, &argv);
@@ -12,36 +12,32 @@ int main(int argc, char *argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	
 	MPI_Request reqs[2];
-	
+	value = rank*2+100;
+	printf("This is rank: %d it has its own value of %d\n", rank, value);
+	fflush(stdout);
 	if (rank == 0) {
-		a = 10;	
-		printf("1This is rank: 0, I have a = %d, b = %d \n", a, b);
-		fflush(stdout);
 		dest = 1;
 	}
-	if (rank == 1) {
-		b = 5;
-		printf("1This is rank: 1, I have a = %d, b = %d \n", a, b);
-		fflush(stdout);
+	else {
 		dest = 0;
-		
 	}
-	MPI_Sendrecv(&b, 1, MPI_INT, dest, tag, &b, 1, MPI_INT, dest, tag, MPI_COMM_WORLD, &Stat);
 	
+	MPI_Sendrecv(&value, 1, MPI_INT, dest, tag, 
+				&rec_val, 1, MPI_INT, dest, tag, 
+				MPI_COMM_WORLD, &Stat);
 
 
-	if (rank == 0) {
 		
-		printf("2This is rank: 0, I have a = %d, b = %d \n", a, b);
-		fflush(stdout);
-	}
-	MPI_Sendrecv(&a, 1, MPI_INT, dest, tag, &a, 1, MPI_INT, dest, tag, MPI_COMM_WORLD, &Stat);
+	printf("This is rank: %d, I have val = %d, rec_val = %d \n", rank, value, rec_val);
+	fflush(stdout);
+	//MPI_Sendrecv(&a, 1, MPI_INT, dest, tag, &a, 1, MPI_INT, dest, tag, MPI_COMM_WORLD, &Stat);
+/*
 	if (rank == 1) {
 		
 		printf("2This is rank: 1, I have a = %d, b = %d \n", a, b);
 		fflush(stdout);
 	}
-
+*/
 	MPI_Finalize();
 
 	return 0;
